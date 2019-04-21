f4bbf4941509ca8329d4e9feef10c41572f47c8b
Trent Davis <trentdavisinc@gmail.com>
2019-04-21 16:12:44 -0500

    Add Sass

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,61 @@
+module.exports = function (app) {
+    // Load index page
+    app.get("/", function (req, res) {
+      res.locals.metaTags = {
+        title: "TDWeb.Dev",
+        description: "Full Stack Web Developer in Austin, Texas",
+        keywords: "web design austin texas"
+      };
+      res.render('index', {"heading": "Full Stack Web Designer & Developer"});
+    });
+
+    app.get("/about", function (req, res) {
+        res.locals.metaTags = {
+          title: "TDWeb.Dev",
+          description: "Full Stack Web Developer in Austin, Texas",
+          keywords: "web design austin texas"
+        };
+        res.render('about');
+      });
+
+      app.get("/contact", function (req, res) {
+        res.locals.metaTags = {
+          title: "TDWeb.Dev",
+          description: "Full Stack Web Developer in Austin, Texas",
+          keywords: "web design austin texas"
+        };
+        res.render('contact');
+      });
+
+      app.get("/portfolio", function (req, res) {
+        res.locals.metaTags = {
+          title: "TDWeb.Dev",
+          description: "Full Stack Web Developer in Austin, Texas",
+          keywords: "web design austin texas"
+        };
+        res.render('portfolio');
+      });
+
+      app.get("/resources", function (req, res) {
+        res.locals.metaTags = {
+          title: "TDWeb.Dev",
+          description: "Full Stack Web Developer in Austin, Texas",
+          keywords: "web design austin texas"
+        };
+        res.render('resources');
+      });
+
+      app.get("/services", function (req, res) {
+        res.locals.metaTags = {
+          title: "TDWeb.Dev",
+          description: "Full Stack Web Developer in Austin, Texas",
+          keywords: "web design austin texas"
+        };
+        res.render('services');
+      });
+
+      app.use(function(req,res){
+        res.status(404);
+        res.render('404');
+    });
+  };
---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+var express = require("express");
+var exphbs = require("express-handlebars");
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Routes
+require("./routes/htmlRoutes")(app);
+
+// Starting the server, ------------------------------------/
+
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+
+
+module.exports = app;
