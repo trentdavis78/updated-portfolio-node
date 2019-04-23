8e2418559a31e0213167d66777090965a333c593
Trent Davis <trentdavisinc@gmail.com>
2019-04-22 22:48:16 -0500

    Start Portfolio page

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,5 @@
 var elem= document.querySelector('.modal');
 var instance = M.Modal.init(elem);
-
-
 var allowedKeys = {
     37: 'left',
     38: 'up',
@@ -9,27 +7,21 @@ var allowedKeys = {
     40: 'down',
     65: 'a',
     66: 'b'
-  };
-  
+  };  
   // the 'official' Konami Code sequence
-  var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
-  
+  var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];  
   // a variable to remember the 'position' the user has reached so far.
-  var konamiCodePosition = 0;
-  
+  var konamiCodePosition = 0;  
   // add keydown event listener
   document.addEventListener('keydown', function(e) {
     // get the value of the key code from the key map
     var key = allowedKeys[e.keyCode];
     // get the value of the required key from the konami code
-    var requiredKey = konamiCode[konamiCodePosition];
-  
+    var requiredKey = konamiCode[konamiCodePosition];  
     // compare the key with the required key
-    if (key == requiredKey) {
-  
+    if (key == requiredKey) {  
       // move to the next key in the konami code sequence
-      konamiCodePosition++;
-  
+      konamiCodePosition++;  
       // if the last key is reached, activate cheats
       if (konamiCodePosition == konamiCode.length) {
         activateCheats();
@@ -38,9 +30,47 @@ var allowedKeys = {
     } else {
       konamiCodePosition = 0;
     }
-  });
-  
+  });  
   function activateCheats() { 
     instance.open();
   }
+  $(document).ready(function() {
+
+
+    $(window).scroll(function(event) {
+  
+        $(".module").each(function(i, el) {
+          var el = $(el);
+          if (el.visible(true)) {
+            el.addClass("come-in"); 
+          } 
+        });
+        
+      });
+      $("#phoneToggle").on("click", function(){
+          if($(this).attr("data-visibility") == "hidden"){
+            $("#phoneNumber").text("512.938.9851");
+            $(this).attr("data-visibility", "visible");
+            $("#phoneToggle").text("Hide");
+          } else {
+            $("#phoneNumber").text("512.*******");
+            $(this).attr("data-visibility", "hidden");
+            $("#phoneToggle").text("Show");
+          }
+      });
+      $("#emailToggle").on("click", function(){
+        if($(this).attr("data-visibility") == "hidden"){
+          $("#emailAddress").html("<a href='mailto:trentdavisinc@gmail.com'>trentdavisinc@gmail.com</a>");
+          $(this).attr("data-visibility", "visible");
+          $("#emailToggle").text("Hide");
+        } else {
+          $("#emailAddress").text("*************@gmail.com");
+          $(this).attr("data-visibility", "hidden");
+          $("#emailToggle").text("Show");
+        }
+    });
+    $(document).ready(function(){
+      $('.collapsible').collapsible();
+    });
+  });
 
