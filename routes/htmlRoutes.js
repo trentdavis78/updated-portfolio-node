7cdbb5fd2533f501f434dd27b66ca9e811e10a73
Trent Davis <trendavisinc@gmail.com>
2019-04-26 00:30:21 -0500

    -

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -37,7 +37,7 @@ module.exports = function (app) {
         };
         Project.find({}, (err, projects) => {
           res.render('portfolio', {projects});
-          console.log(projects);
+          console.log("response of: " + projects);
         });
         
       });
